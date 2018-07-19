ca68556fe438e74c456b36d82dc2bdb8e7bc45bf
Adam Czajkowski <adam.czajkowski8@gmail.com>
2018-07-20 01:38:23 +0200

    first commit

---
+++ Bottle.java
@@ -0,0 +1,20 @@
+package bottles;
+
+public class Bottle {
+    private double capacityOfBottle;
+    private double quantityInBottle;
+
+    Bottle(double quantityInBottle) {
+            this.quantityInBottle = quantityInBottle;
+    }
+
+    boolean pourOut(double quantity) {
+
+        return true;
+    }
+
+    boolean  pour(double quantity) {
+    return true;
+    }
+
+}
